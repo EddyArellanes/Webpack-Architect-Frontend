bf5c575c743a030df689dae15a7eea8cde012e5b
Eddy Arellanes Bastida <eddyarellanes.developer@gmail.com>
2018-06-19 00:42:30 -0500

    playing with the styles

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,7 @@
 import "../css/app.scss"
 //Using import to test traspiling babel
 import { eddyAB } from './messages.js'
+import zeroImg from "../img/zero.png";
 
-console.log(eddyAB)
+let zero = document.getElementById('zero')
+zero.setAttribute('src', zeroImg)
