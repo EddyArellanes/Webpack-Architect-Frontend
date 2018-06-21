7803715e5b067ac90e8d1d8f448e5689e570cbc5
Eddy <eddy@hotmail.com>
2018-06-21 18:05:21 -0500

    added fonts and video support

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,16 @@
 import "../css/app.scss"
 //Using import to test traspiling babel
 import { eddyAB } from './messages.js'
-import zeroImg from "../img/zero.png";
+import zeroImg from "../img/zero.png"
+import testVideo from "../videos/Core.mp4"
 
 let zero = document.getElementById('zero')
 zero.setAttribute('src', zeroImg)
+
+let video = document.createElement('video')
+video.setAttribute('src', testVideo)
+video.setAttribute('width',480)
+video.setAttribute('autoplay',480)
+video.setAttribute('controls', true)
+document.body.append(video)
+
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+        filename: 'js/bundle.js',
+        publicPath: './dist/'
     },
     devServer: {       
         contentBase: './dist',
@@ -27,11 +28,24 @@ module.exports = {
             //Javascript Loaders
             //It's a little more complex than css
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/,
                 use: {
                     loader: 'url-loader',
-                    options: {                        
-                        limit: 2097152,
+                    options: {
+                        // 2 Megabytes                  
+                        limit: 2097152,                         
+                        name: 'img/[name].ext'
+                    }
+                }
+            },
+            {
+                test: /\.(webm|mp4)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {      
+                        //1 Megabyte                           
+                        limit: 1048576,
+                        name: 'videos/[name].[hash].[ext]'
                     }
                 }
             },
@@ -91,4 +105,14 @@ babel-preset-env: Targets the ECMA version to transpile
 Images
 url-loader : A loader for webpack which transforms files into base64 URIs. Useful for optimize css and js when you need  to import files
 file-loader: Specifies an alternative loader to use when a target file's size exceeds the limit set in the limit option. maximum size of a file in bytes
+
+Videos
+Repeat the code of images only change types and limit
+
+Fonts
+To support fonts in the process of webpack the only thing we need is in url-loader add
+woff|eot|ttf|svg
+I have downloaded fonts from
+https://www.fontsquirrel.com specify open sans webkit to support to display font without the need for the user to have it installed
 */
+
